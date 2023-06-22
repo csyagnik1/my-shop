fb0ea813cdea1efaadcdf0e05bfb53c08dd9c001
csyagnik1 <csyagnik@gmail.com>
2023-06-22 10:38:36 +0530

    updated mycorousel

--- src/components/Mycorousel.js
+++ src/components/Mycorousel.js
@@ -24,25 +24,23 @@ const Mycorousel = () => {
     };
     return (
 
-        <div className='banner' style={{"text-align": "center" }} >
-            <Carousel 
-            responsive={responsive}
-            
-            autoPlay={true} 
-            autoPlaySpeed={3000}
-            rewind={true}
-            rewindWithAnimation={true}
-                             
-                      
-            showDots={true}>
-                <div><img src='images/corousel/1.png' alt='img' style={{"width": "100%" }}/>  </div>
-                <div><img src='images/corousel/2.png' alt='img' style={{"width": "100%" }}/> </div>
-                <div><img src='images/corousel/3.png' alt='img' style={{"width": "100%" }}/> </div>
-                <div><img src='images/corousel/4.png' alt='img' style={{"width": "100%" }}/> </div>
-                <div><img src='images/corousel/5.png' alt='img' style={{"width": "100%" }}/> </div>
-                <div><img src='images/corousel/6.png' alt='img' style={{"width": "100%" }}/> </div>
-                
-                
+        <div className='banner' style={{ "text-align": "center" }} >
+            <Carousel
+                responsive={responsive}
+                autoPlay={true}
+                autoPlaySpeed={3000}
+                rewind={true}
+                rewindWithAnimation={true}
+                showDots={true}>
+
+                <div><img src='images/corousel/1.png' alt='img' style={{ "width": "100%" }} />  </div>
+                <div><img src='images/corousel/2.png' alt='img' style={{ "width": "100%" }} /> </div>
+                <div><img src='images/corousel/3.png' alt='img' style={{ "width": "100%" }} /> </div>
+                <div><img src='images/corousel/4.png' alt='img' style={{ "width": "100%" }} /> </div>
+                <div><img src='images/corousel/5.png' alt='img' style={{ "width": "100%" }} /> </div>
+                <div><img src='images/corousel/6.png' alt='img' style={{ "width": "100%" }} /> </div>
+
+
             </Carousel>;
 
         </div>
